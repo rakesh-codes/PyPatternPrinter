b5b84da60e0cb4ae88402df3c58e68f15febbfc8
Rakesh <132572472+rakesh-codes@users.noreply.github.com>
2023-09-09 20:58:07 +0530

    Update pattern_1.py

--- All_pattern/pattern_1.py
+++ All_pattern/pattern_1.py
@@ -1,6 +1,16 @@
+#code
 n=int(input("please enter the number of lines to print: " ))
 
 for i in range(n):
     for j in range(n):
         print(f" * ", end=" ")
     print()
+
+#this is the output
+Pattern-1:
+
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
